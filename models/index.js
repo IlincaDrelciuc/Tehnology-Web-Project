7fb11f72bc7d485552eeb8d17f12143b7511608c
IlincaDrelciuc <drelciucilinca23@stud.ase.ro>
2026-01-07 15:21:44 +0200

    Backend

--- models/index.js
+++
@@ -1,8 +0,0 @@
-const sequelize = require('../database');
-const User = require('./User');
-const Item = require('./Item');
-
-User.hasMany(Item, { foreignKey: 'user_id' });
-Item.belongsTo(User, { foreignKey: 'user_id' });
-
-module.exports = { sequelize, User, Item };
